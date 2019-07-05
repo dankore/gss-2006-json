41f53de5783cc7a12fa5267830969d1fa52496de
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-07-04 20:09:33 -0500

    Update and rename 2006-Set to 2006.js

---
+++ 2006.js
@@ -0,0 +1,46 @@
+Template
+{
+    "name": "",
+    "class": "",
+    "state": "",
+    "occupation": "",
+    "dob": "",
+  },
+
+[
+  {
+    "name": "Adamu Dankore Muhammad",
+    "class": "JS1-3F, SS1-3A",
+    "state": "Iowa, USA",
+    "occupation": "Chemist, Founder & CEO https://homeawayfromhome.online",
+    "dob": "July 2",
+  },
+  {
+    "name": "Audu Nurudeen",
+    "class": "SS1F, SS3E",
+    "state": "Kubwa, Abuja",
+    "occupation": "National Orientation Agency, Tax Consultant, Media personnel",
+    "dob": "December 14",
+  },
+  {
+    "name": "Kalu A Victoria",
+    "class": "SS3A",
+    "state": "Abuja",
+    "occupation": "",
+    "dob": "Novemeber 27",
+  },
+  {
+    "name": "Samuel Sunday",
+    "class": "SS1D",
+    "state": "Abuja",
+    "occupation": "",
+    "dob": "",
+  },
+  {
+    "name": "Moses Okorogbudje",
+    "class": "SS3E",
+    "state": "Abuja",
+    "occupation": "Estate Management Agent, Emcee, Writer & Business man",
+    "dob": "September 21",
+  },
+]
